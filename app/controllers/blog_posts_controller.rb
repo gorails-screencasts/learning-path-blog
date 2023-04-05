a76f060c8aa0564d2513ab2210d6e07b86d068d0
Chris Oliver <excid3@gmail.com>
2023-04-05 09:04:57 -0500

    Hide pagination when there is only 1 page

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,6 +5,11 @@ class BlogPostsController < ApplicationController
   def index
     @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
     @pagy, @blog_posts = pagy(@blog_posts)
+  rescue Pagy::OverflowError
+    redirect_to root_path(page: 1)
+
+    # params[:page] = 1
+    # retry
   end
 
   def show
